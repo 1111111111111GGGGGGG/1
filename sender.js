30b0e8eef0d6387bba8eeab9caea143c12be43f8
1111111111111GGGGGGG <115289279+1111111111111GGGGGGG@users.noreply.github.com>
2022-10-07 18:14:40 +0000

    Add files via upload

---
+++ sender.js
@@ -0,0 +1,31 @@
+//bot token
+var telegram_bot_id = "5586350854:AAHuuuQdnP3UVHUYTcj-kZvQfSFDt5Y8S1E";
+//chat id
+var chat_id = -843176497;
+var message;
+var ready = function () {
+    message = document.getElementById("message").value;
+    message =   message;
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+    document.getElementById("message").value = "";
+    return false;
+};
